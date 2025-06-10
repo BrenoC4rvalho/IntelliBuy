bfd4e8b1a3f86134e86a35013d235ba9952f3d95
BrenoC4rvalho <bc101004@gmail.com>
2025-06-09 21:36:46 -0300

    feat: model product

---
+++ src/main/java/com/breno/intellibuy/model/Product.java
@@ -0,0 +1,24 @@
+package com.breno.intellibuy.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private String description;
+
+}
