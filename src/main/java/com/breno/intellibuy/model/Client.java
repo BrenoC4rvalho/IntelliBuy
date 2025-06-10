92ac13024765c22fecc2bd816c7915b56191d39c
BrenoC4rvalho <bc101004@gmail.com>
2025-06-09 21:36:33 -0300

    feat: model client

---
+++ src/main/java/com/breno/intellibuy/model/Client.java
@@ -0,0 +1,22 @@
+package com.breno.intellibuy.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private String phone;
+
+}
