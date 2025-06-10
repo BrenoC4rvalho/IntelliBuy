e54d4329d96fe1032c802023ad7dca851a6ec65a
BrenoC4rvalho <bc101004@gmail.com>
2025-06-09 21:28:40 -0300

    first commit

---
+++ src/main/java/com/breno/intellibuy/IntellibuyApplication.java
@@ -0,0 +1,13 @@
+package com.breno.intellibuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellibuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellibuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/breno/intellibuy/IntellibuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breno.intellibuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellibuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
