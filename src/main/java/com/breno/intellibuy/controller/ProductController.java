96ebd687d0314a0fd6cacc3f1897308aa82ad7e4
BrenoC4rvalho <bc101004@gmail.com>
2025-06-12 20:53:07 -0300

    feat: edit product controller

--- src/main/java/com/breno/intellibuy/controller/ProductController.java
+++ src/main/java/com/breno/intellibuy/controller/ProductController.java
@@ -1,50 +1,56 @@
 package com.breno.intellibuy.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.breno.intellibuy.model.Product;
+import com.breno.intellibuy.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
     @Autowired
-    private ProdutoService produtoService;
+    private ProductService productService;
 
     @PostMapping
-    public ResponseEntity<Produto> createProduto(@RequestBody Produto produto) {
-        Produto savedProduto = produtoService.saveProduto(produto);
-        return new ResponseEntity<>(savedProduto, HttpStatus.CREATED);
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product savedProduct = productService.save(product);
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<List<Produto>> getAllProdutos() {
-        List<Produto> produtos = produtoService.getAllProdutos();
-        return new ResponseEntity<>(produtos, HttpStatus.OK);
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAll();
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Produto> getProdutoById(@PathVariable Long id) {
-        return produtoService.getProdutoById(id)
-                .map(produto -> new ResponseEntity<>(produto, HttpStatus.OK))
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return productService.getById(id)
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Produto> updateProduto(@PathVariable Long id, @RequestBody Produto produto) {
-        return produtoService.getProdutoById(id)
-                .map(produtoExistente -> {
-                    produto.setId(id);
-                    Produto updatedProduto = produtoService.saveProduto(produto);
-                    return new ResponseEntity<>(updatedProduto, HttpStatus.OK);
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return productService.getById(id)
+                .map(existProduct -> {
+                    product.setId(id);
+                    Product updatedProduct = productService.save(product);
+                    return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
                 })
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduto(@PathVariable Long id) {
-        return produtoService.getProdutoById(id)
-                .map(produto -> {
-                    produtoService.deleteProduto(id);
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+        return productService.getById(id)
+                .map(product -> {
+                    productService.delete(id);
                     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
                 })
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
